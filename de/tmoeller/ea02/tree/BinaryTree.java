63f7ca03b5ddededcb9aaae16029c063345419ef
Torsten <76787943+tmoOps@users.noreply.github.com>
2021-04-15 19:10:06 +0200

    EA03-A3 Binary Tree

--- de/tmoeller/ea02/tree/BinaryTree.java
+++ de/tmoeller/ea02/tree/BinaryTree.java
@@ -2,4 +2,42 @@ package tree;
 
 public class BinaryTree {
 
-}
+	private static class BinaryNode{
+		private BinaryNode leftSon, rightSon;
+		
+		private int value;
+		
+		public BinaryNode(int value) {
+			this.value = value;
+		}//end of BinaryNode
+				
+	}//end of class BinaryNode
+	
+	
+	private BinaryNode root;
+		
+	
+	public BinaryTree() {
+		
+	}//end of BinaryTree
+	
+	
+	public BinaryTree(int value) {
+		
+	}//end of BinaryTree constructor
+	
+	
+	public boolean contains (int value) {
+		return true;
+	}//end of contains
+	
+	public void insert(int value) {
+		
+	}//end of insert
+	
+	public void inorder() {
+		
+	}//end of inorder
+		
+	
+}//end of class BinaryTree
