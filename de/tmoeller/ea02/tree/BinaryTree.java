e0c8d0234b773196dc9ce3e5ca917d51c93312f8
Torsten <76787943+tmoOps@users.noreply.github.com>
2021-04-16 03:40:37 +0200

    EA02-A3 Ausimplementierung Binary Tree

--- de/tmoeller/ea02/tree/BinaryTree.java
+++ de/tmoeller/ea02/tree/BinaryTree.java
@@ -7,36 +7,172 @@ public class BinaryTree {
 		
 		private int value;
 		
-		public BinaryNode(int value) {
-			this.value = value;
+		BinaryNode(int value) {
+			this.value = value;		
 		}//end of BinaryNode
+		
+		//setter Methoden
+		void setLeft( BinaryNode n ) {
+			this.leftSon = n;
+		}//end of setLeft
+		
+		void setRight( BinaryNode n) {
+			this.rightSon = n;
+		}//end of setRight
+		
+		//getter Methoden
+		BinaryNode getLeft() {
+			return this.leftSon;
+		}//end getLeft
+		
+		BinaryNode getRight() {
+			return this.rightSon;
+		}//end getRight		
 				
 	}//end of class BinaryNode
 	
 	
-	private BinaryNode root;
-		
+	private BinaryNode root, nullNode;
+	private String resStr;
 	
 	public BinaryTree() {
 		
+		/* erzeugen eines leeren Baums
+		
+		 * nullNode ist ein Hilfsobjekt und repräsentiert
+		 * eine "Pseudoknoten", die leere Verweise und die Blätter
+		 * des Baumes kennzeichnen
+		*/
+		
+		//Pseudoknoten erzeugen
+		nullNode = new BinaryNode( 0 );
+		nullNode.setRight( nullNode );
+		nullNode.setLeft( nullNode );
+					
 	}//end of BinaryTree
 	
 	
 	public BinaryTree(int value) {
+
+		/* erzeugen eines Baums, der lediglich den Wurzelknoten root
+		 * enthält
+		 */
+		
+		//Pseudoknoten erzeugen
+		nullNode = new BinaryNode( 0 );
+		nullNode.setRight( nullNode );
+		nullNode.setLeft( nullNode );
+		
+		root = new BinaryNode( value );
+		root.setLeft( nullNode );
+		root.setRight( nullNode );
 		
 	}//end of BinaryTree constructor
 	
 	
 	public boolean contains (int value) {
+		
+		/* Idee
+		 * Baum rekursiv durchlaufen und den Inhalt
+		 * eines Knotens mit value vergleichen
+		 * 
+		 * Traversierung beginnt immer an der Wurzel root
+		 * von dort verästelt sich der Baum nach links und rechts
+		 * in 2 Teilbäume
+		 * 
+		 */
+		
+		
 		return true;
 	}//end of contains
 	
-	public void insert(int value) {
+	
+	public void insert( int value ) {
+				
+		/* Abfrage, ob ein Wurzelknoten bereits vorhanden ist
+		*/
+		if( root == null ) {
+		
+			/* Wurzel nicht vorhanden, 
+			 * dann Wurzel neu anlegen
+			*/ 
+			root = new BinaryNode( value );
+			root.setLeft( nullNode );
+			root.setRight( nullNode );
+			
+			return;
+		}//end if
+		
+		/*
+		 * Wurzel vorhanden
+		 */
+
+		//neuen Blattknoten anlegen
+		BinaryNode newNode = new BinaryNode( value );
+		newNode.setLeft( nullNode );
+		newNode.setRight( nullNode );
+			
+		BinaryNode compareNode = root;
+		
+		while( compareNode != nullNode ){
+			
+			if( value < compareNode.value ) {
+				
+				if( compareNode.getLeft() == nullNode ) {
+					compareNode.setLeft( newNode );
+					return;
+				}//end if
+				else {
+					compareNode = compareNode.getLeft();
+				}//end else
+			
+			}//end if
+			else {
+				
+				if( value > compareNode.value ) {
+					
+					if( compareNode.getRight() == nullNode ) {
+						compareNode.setRight( newNode );
+						return;
+					}//end if
+					else {
+						compareNode = compareNode.getRight();
+					}//end else
+										
+				}//end if
+				else {
+				  return;
+				}//end else
+			
+			}//end else
+			
+				
+		}//end while
+						
 		
 	}//end of insert
+		
+	
+	private void inorderRek( BinaryNode k ) {
+		//rekursives Durchlaufen des Baumes
+		if( k != nullNode ) {
+			
+			//Teilbaum links
+			inorderRek( k.getLeft() );
+		
+			resStr += Integer.toString( k.value ) + " -> ";
+		
+			//Teilbaum rechts
+			inorderRek( k.getRight() );
+		}//end if
+			
+	}//end of inorderRek
+	
 	
 	public void inorder() {
-		
+		resStr = new String();
+		inorderRek( root );
+		System.out.println( resStr );		
 	}//end of inorder
 		
 	
--- de/tmoeller/ea02/tree/TestTree.java
+++ de/tmoeller/ea02/tree/TestTree.java
@@ -5,6 +5,16 @@ public class TestTree {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		BinaryTree myTree = new BinaryTree(6);
+		myTree.insert(5);
+		myTree.insert(4);
+		myTree.insert(12);
+		myTree.insert(11);
+		myTree.insert(10);
+		//myTree.insert(7);
+		//myTree.insert(8);
+		myTree.inorder();	
+				
+	}//end of main
 
-}
+}//end of TestTree
